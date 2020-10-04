1d6c6bae0ad32c18e78ab11d21c4f59dee057894
bryancalisto@yahoo.es <bryancalisto@yahoo.es>
2020-10-04 18:39:47 -0500

    new: added reports page

---
+++ eflutter/lib/reports/reportsPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ReportsPage extends StatefulWidget {
+  @override
+  _ReportsPageState createState() => _ReportsPageState();
+}
+
+class _ReportsPageState extends State<ReportsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('REPORT_NAME'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
