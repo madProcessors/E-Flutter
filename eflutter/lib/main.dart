7c91da6007da64faeb822a79a8d0a4abb5d1357d
bryancalisto@yahoo.es <bryancalisto@yahoo.es>
2021-01-03 19:52:22 -0500

    new: create main.dart

--- eflutter/lib/main.dart
+++ eflutter/lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
-import './login/loginPage.dart';
+import './profile/profilePage.dart';
+import './models/User.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-<<<<<<< HEAD
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var user = User();
+
   @override
   Widget build(BuildContext context) {
+    user.username = 'BRYAN';
+    user.email = 'BRYAN@asd.com';
     return MaterialApp(
-      title: 'E-Flutter',
       home: Scaffold(
-        body: LoginPage(),
+        body: ProfilePage(user),
       ),
     );
   }
 }
-=======
->>>>>>> 5909ddf5c7f160cc8ba02eaffd7547482b4e135f
